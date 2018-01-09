bcc90cd10a45f3660a762f7b3ca4eebe01655929
Lero <2632119760@qq.com>
2018-01-09 23:56:45 +0800

    添加了home动画

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/throttleTime';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private rotateSubject: Subject<string>;
+  rotate3d: Observable<string>;
+  constructor() {
+    this.rotateSubject = new Subject();
+    this.rotate3d = this.rotateSubject.throttleTime(500);
+  }
 
   ngOnInit() {
   }
 
+  handleMouseMove(e: MouseEvent) {
+    let rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const halfWidth = rect.width / 2;
+    const halfHeight = rect.height / 2;
+    const pencentageY = (x - halfWidth) / halfWidth;
+    const pencentageX = (y - halfHeight) / halfHeight;
+    const centerX = rect.left + rect.width / 2,
+          centerY = rect.top + rect.height / 2;
+    const deltaX = e.clientX - centerX,
+          deltaY = e.clientY - centerY;
+    const percentageX = deltaX / centerX,
+          percentageY = deltaY / centerY;
+    const deg = 10;
+    const rotate3d = `rotateX(${percentageY * -10}deg) rotateY(${percentageX * 10}deg)`;
+    this.rotateSubject.next(rotate3d);
+  }
+  handleMouseOut() {
+    const rotate3d = `rotateX(0deg) rotateY(0deg)`;
+    this.rotateSubject.next(rotate3d);
+  }
 }
